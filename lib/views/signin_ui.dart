a51fc2a134fd1d30cd45b1776e1bdcefd581a1e5
DanaiLathong <bankkok159z@gmail.com>
2025-01-15 12:33:46 +0700

    V.2 สร้างไฟล์หน้าจอไป 3 ไฟล์ แก้ไข Main แล้วก็ทำหน้าจอ welcome เสร็จแล้ว

---
+++ lib/views/signin_ui.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SigninUi extends StatefulWidget {
+  const SigninUi({super.key});
+
+  @override
+  State<SigninUi> createState() => _SigninUiState();
+}
+
+class _SigninUiState extends State<SigninUi>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
